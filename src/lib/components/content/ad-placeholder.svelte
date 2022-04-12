2a0c8d650f191a78a99c8bd38ead7db1d5a567f2
Lucas Fernandes <me@lucasfernandes.com.br>
2022-04-12 00:08:55 -0300

    Add AdPlaceholder

---
+++ src/lib/components/content/ad-placeholder.svelte
@@ -0,0 +1,16 @@
+<div class="publi-placeholder">Publicidade</div>
+
+<style lang="sass">
+  // This class name is to dodge adblockers
+  .publi-placeholder
+    display: flex
+    justify-content: center
+    align-items: center
+    width: 100%
+    height: 190px
+    background-color: #ebeff1
+    color: rgba(#000, .2)
+    font-size: 2rem
+    font-weight: bold
+    border-radius: 10px
+</style>
