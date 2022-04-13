d575f03ff3b928f448de4906c993a21d0e0fbe22
Lucas Fernandes <me@lucasfernandes.com.br>
2022-04-13 03:55:19 -0300

    Fix links

--- src/lib/modules/article/articles.store.ts
+++ src/lib/modules/article/articles.store.ts
@@ -2,9 +2,17 @@ import { categoryColors } from '.';
 import { getArticle, getHeadlines, getOtherArticles } from './articles.service';
 import type { ArticleDetails, Article } from '$lib/components/content/article';
 
+const getLink = (slug: string, id: number) => {
+  if (slug == null || id == null) return;
+
+  const parts = slug.split('-');
+  const lastPart = parts[parts.length - 1];
+  return lastPart == id.toString() ? `/${slug}` : `/${slug}-${id}`;
+};
+
 const parseArticle = (article: ArticleDetails) => ({
   ...article,
-  link: `/${article.slug}`,
+  link: getLink(article.slug, article.id),
   color: categoryColors[article.categoryId],
 });
 
