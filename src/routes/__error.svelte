a4c76a46e1d935293295dd029d8a22627606d261
Lucas Fernandes <me@lucasfernandes.com.br>
2022-04-13 03:12:01 -0300

    Add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: {
+        status,
+        message: error.message,
+      },
+    };
+  }
+</script>
+
+<script lang="ts">
+  import { TextBodyMedium, TextTitleLarge } from '$lib/components/text';
+
+  export let status: number, message: string;
+</script>
+
+<div class="container">
+  <TextTitleLarge>{status}</TextTitleLarge>
+  <TextBodyMedium>{message}</TextBodyMedium>
+</div>
+
+<style lang="sass">
+  .container
+    text-align: center
+</style>
