ba9d8b3fb6c188396f6ce7536ed85e115f9c0add
Lucas Fernandes <me@lucasfernandes.com.br>
2022-04-13 01:39:34 -0300

    Add TextSubtitleMedium

--- src/lib/components/text/index.ts
+++ src/lib/components/text/index.ts
@@ -1,4 +1,5 @@
 export { default as TextCategoryLabel } from './text-category-label.svelte';
+export { default as TextSubtitleMedium } from './text-subtitle-medium.svelte';
 export { default as TextTitleLarge } from './text-title-large.svelte';
 export { default as TextTitleMedium } from './text-title-medium.svelte';
 export { default as TextTitleSmall } from './text-title-small.svelte';
---
+++ src/lib/components/text/text-subtitle-medium.svelte
@@ -0,0 +1,8 @@
+<p>
+  <slot />
+</p>
+
+<style lang="sass">
+  p
+    font-size: 1.5rem
+</style>
